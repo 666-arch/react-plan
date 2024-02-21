eb281dd7ffeab8f097a5fc271407731fa96d65a0
MaxHuang <hc18174419502@163.com>
2024-02-21 17:23:01 +0800

    useState2

--- src/02_计数器实现对比/CounterHook.jsx
+++ src/02_计数器实现对比/CounterHook.jsx
@@ -4,8 +4,8 @@ const CounterHook = () => {
     const [counter, setCounter] = useState(0)
     return <div>
         <h2>当前计数：{counter}</h2>
-        <button onClick={e=>setCounter(100)}>+1</button>
-        <button onClick={e=>setCounter(-100)}>-1</button>
+        <button onClick={e=>setCounter(counter+1)}>+1</button>
+        <button onClick={e=>setCounter(counter-1)}>-1</button>
     </div>    
 }
 
