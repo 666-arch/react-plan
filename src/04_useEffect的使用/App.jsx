2e66b489b4b497df70fced0fc68c7a93650b0acb
MaxHuang <hc18174419502@163.com>
2024-02-22 10:36:30 +0800

    use useEffect update class

--- src/04_useEffect的使用/App.jsx
+++ src/04_useEffect的使用/App.jsx
@@ -1,9 +1,27 @@
-import React, { memo } from 'react'
+import React, { PureComponent } from 'react'
 
-export default memo(function App() {
-  return (
-    <div>
-      App
-    </div>
-  )
-})
+export class App extends PureComponent {
+  constructor(){
+    super()
+    this.state={
+      counter: 100
+    }
+  }
+  componentDidMount(){
+    document.title = this.state.counter
+  }
+  componentDidUpdate(){
+    document.title = this.state.counter
+  }
+  render() {
+    const {counter} = this.state
+    return (
+      <div>
+        <h2>计数：{counter}</h2>
+        <button onClick={e=>this.setState({counter: counter+1})}>+1</button>
+      </div>
+    )
+  }
+}
+
+export default App
